d482c0cf8f841dd083541fd4ec303c383e88b08b
Sigvart Hovland <sigvart.hovland@nordicsemi.no>
2018-11-05 16:58:29 +0100

    add vendor vectors

--- subsys/bootloader/c_runtime_setup/startup.c
+++ subsys/bootloader/c_runtime_setup/startup.c
@@ -53,7 +53,6 @@ extern uint32_t _image_text_end;
 
 /* C main function to be called from the reset_handler */
 extern int main(void);
-//int main(void);
 /* Device specific intialization functions for erratas and system clock setup */
 extern void SystemInit(void);
 /* Forward decleration for dummy handler */
@@ -121,6 +120,164 @@ void *core_vector_table[16] __attribute__((section(".exc_vector_table"))) = {
 #else
 	0,		       /* reserved */
 #endif /* CONFIG_CORTEX_M_SYSTICK */
+};
+
+void POWER_CLOCK_IRQHandler(void) ALIAS("dummy_handler");
+void RADIO_IRQHandler(void) ALIAS("dummy_handler");
+void UARTE0_UART0_IRQHandler(void) ALIAS("dummy_handler");
+void SPI0_SPIS0_TWIM0_TWIS0_SPI0_TWI0_IRQHandler(void) ALIAS("dummy_handler");
+void SPI1_SPIS1_TWIM1_TWIS1_SPI1_TWI1_IRQHandler(void) ALIAS("dummy_handler");
+void NFCT_IRQHandler(void) ALIAS("dummy_handler");
+void GPIOTE_IRQHandler(void) ALIAS("dummy_handler");
+void SAADC_IRQHandler(void) ALIAS("dummy_handler");
+void TIMER0_IRQHandler(void) ALIAS("dummy_handler");
+void TIMER1_IRQHandler(void) ALIAS("dummy_handler");
+void TIMER2_IRQHandler(void) ALIAS("dummy_handler");
+void RTC0_IRQHandler(void) ALIAS("dummy_handler");
+void TEMP_IRQHandler(void) ALIAS("dummy_handler");
+void RNG_IRQHandler(void) ALIAS("dummy_handler");
+void ECB_IRQHandler(void) ALIAS("dummy_handler");
+void CCM_AAR_IRQHandler(void) ALIAS("dummy_handler");
+void WDT_IRQHandler(void) ALIAS("dummy_handler");
+void RTC1_IRQHandler(void) ALIAS("dummy_handler");
+void QDEC_IRQHandler(void) ALIAS("dummy_handler");
+void COMP_LPCOMP_IRQHandler(void) ALIAS("dummy_handler");
+void SWI0_EGU0_IRQHandler(void) ALIAS("dummy_handler");
+void SWI1_EGU1_IRQHandler(void) ALIAS("dummy_handler");
+void SWI2_EGU2_IRQHandler(void) ALIAS("dummy_handler");
+void SWI3_EGU3_IRQHandler(void) ALIAS("dummy_handler");
+void SWI4_EGU4_IRQHandler(void) ALIAS("dummy_handler");
+void SWI5_EGU5_IRQHandler(void) ALIAS("dummy_handler");
+void TIMER3_IRQHandler(void) ALIAS("dummy_handler");
+void TIMER4_IRQHandler(void) ALIAS("dummy_handler");
+void PWM0_IRQHandler(void) ALIAS("dummy_handler");
+void PDM_IRQHandler(void) ALIAS("dummy_handler");
+void MWU_IRQHandler(void) ALIAS("dummy_handler");
+void PWM1_IRQHandler(void) ALIAS("dummy_handler");
+void PWM2_IRQHandler(void) ALIAS("dummy_handler");
+void SPIM2_SPIS2_SPI2_IRQHandler(void) ALIAS("dummy_handler");
+void RTC2_IRQHandler(void) ALIAS("dummy_handler");
+void I2S_IRQHandler(void) ALIAS("dummy_handler");
+void FPU_IRQHandler(void) ALIAS("dummy_handler");
+void USBD_IRQHandler(void) ALIAS("dummy_handler");
+void UARTE1_IRQHandler(void) ALIAS("dummy_handler");
+void QSPI_IRQHandler(void) ALIAS("dummy_handler");
+void CRYPTOCELL_IRQHandler(void) ALIAS("dummy_handler");
+void PWM3_IRQHandler(void) ALIAS("dummy_handler");
+void SPIM3_IRQHandler(void) ALIAS("dummy_handler");
+
+void *vendor_vector_table[] __attribute__ ((section(".gnu.linkonce.irq_vector_table"))) = {
+POWER_CLOCK_IRQHandler,
+RADIO_IRQHandler,
+UARTE0_UART0_IRQHandler,
+SPI0_SPIS0_TWIM0_TWIS0_SPI0_TWI0_IRQHandler,
+SPI1_SPIS1_TWIM1_TWIS1_SPI1_TWI1_IRQHandler,
+NFCT_IRQHandler,
+GPIOTE_IRQHandler,
+SAADC_IRQHandler,
+TIMER0_IRQHandler,
+TIMER1_IRQHandler,
+TIMER2_IRQHandler,
+RTC0_IRQHandler,
+TEMP_IRQHandler,
+RNG_IRQHandler,
+ECB_IRQHandler,
+CCM_AAR_IRQHandler,
+WDT_IRQHandler,
+RTC1_IRQHandler,
+QDEC_IRQHandler,
+COMP_LPCOMP_IRQHandler,
+SWI0_EGU0_IRQHandler,
+SWI1_EGU1_IRQHandler,
+SWI2_EGU2_IRQHandler,
+SWI3_EGU3_IRQHandler,
+SWI4_EGU4_IRQHandler,
+SWI5_EGU5_IRQHandler,
+TIMER3_IRQHandler,
+TIMER4_IRQHandler,
+PWM0_IRQHandler,
+PDM_IRQHandler,
+0,
+0,
+MWU_IRQHandler,
+PWM1_IRQHandler,
+PWM2_IRQHandler,
+SPIM2_SPIS2_SPI2_IRQHandler,
+RTC2_IRQHandler,
+I2S_IRQHandler,
+FPU_IRQHandler,
+USBD_IRQHandler,
+UARTE1_IRQHandler,
+QSPI_IRQHandler,
+CRYPTOCELL_IRQHandler,
+0,
+0,
+PWM3_IRQHandler,
+0,
+SPIM3_IRQHandler,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
 };
 
 void _bss_zero(uint32_t *dest, uint32_t *end)
