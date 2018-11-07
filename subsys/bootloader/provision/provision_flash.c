de55b8b4cc228d29ec68895d1c4417b54e296115
Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>
2018-11-07 08:01:55 +0000

    Simplified access to provision data.

--- subsys/bootloader/provision/provision_flash.c
+++ subsys/bootloader/provision/provision_flash.c
@@ -13,26 +13,23 @@ typedef struct {
 	uint8_t  pkd[CONFIG_SB_NUM_PUBLIC_KEY_HASHES * PK_LEN];
 } provision_flash_t;
 
-
-/* This symbol is stored at the page memory region named 'provision' in dts. 
- * It works as a placeholder here, and is filled by a python script 'provision.py'. */
-__attribute__ ((section(".PROVISION_DATA"))) volatile const provision_flash_t provision_data;
+static const provision_flash_t * p_provision_data = (provision_flash_t *)FLASH_AREA_PROVISION_OFFSET;
 
 uint32_t s0_address_read(void)
 {
-	return provision_data.s0_address;
+	return p_provision_data->s0_address;
 }
 
 uint32_t s1_address_read(void)
 {
-	return provision_data.s1_address;
+	return p_provision_data->s1_address;
 }
 
 const uint8_t * public_key_data_read(uint32_t key_index)
 {
 	uint32_t i = 0;
 	for(i = 0; i < PK_LEN; ++i) {
-		key_buf[i] = provision_data.pkd[key_index*PK_LEN + i];
+		key_buf[i] = p_provision_data->pkd[key_index*PK_LEN + i];
 	}
 	return key_buf;
 }
