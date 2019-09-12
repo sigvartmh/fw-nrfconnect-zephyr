e43e8f114b16bd6660ba008e90e636edb68672a8
Sigvart Hovland <sigvart.m@gmail.com>
2019-09-12 21:54:01 +0200

    GNUARMEMB PATH

--- cmake/toolchain/llvm/generic.cmake
+++ cmake/toolchain/llvm/generic.cmake
@@ -17,10 +17,10 @@ set(LINKER ld) # TODO: Use lld eventually rather than GNU ld
 set(BINTOOLS llvm)
 
 if("${ARCH}" STREQUAL "arm")
-  set(triple arm-zephyr-eabi)
+  set(triple arm-none-eabi)
   set(CMAKE_EXE_LINKER_FLAGS_INIT "--specs=nosys.specs")
   set_ifndef(GNUARMEMB_TOOLCHAIN_PATH "$ENV{GNUARMEMB_TOOLCHAIN_PATH}")
-  find_program(ARMGCC ${triple}-gcc PATHS ${ZEPHYR_SDK_INSTALL_DIR}/${triple}/bin NO_DEFAULT_PATH)
+  find_program(ARMGCC ${triple}-gcc PATHS ${GNUARMEMB_TOOLCHAIN_PATH}/bin NO_DEFAULT_PATH)
   assert_exists(ARMGCC)
   set(CMAKE_C_LINK_EXECUTABLE "${ARMGCC} <FLAGS> <CMAKE_C_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES> -lc")
   set(CMAKE_CXX_LINK_EXECUTABLE "${ARMGCC} <FLAGS> <CMAKE_C_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES> -lc")
